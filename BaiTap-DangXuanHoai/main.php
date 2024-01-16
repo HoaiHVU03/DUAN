8f35c584af2a21964e274b0121a3bf0c9e0fd41c
HoaiHVU03 <hoai0386574833@gmail.com>
2024-01-16 15:07:42 +0700

    Web-test-seoulmobile

---
+++ BaiTap-DangXuanHoai/main.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Web Mobile</title>
+</head>
+<body>
+    <div class="banner">
+        <div class="logo">
+            <img src="./img/LOGO.jpg">
+        </div>
+        <div class="hotline">
+            <span>Holine: 0386574833 - 0386265210</span> <br>
+            <span>Địa chỉ: Võ Miếu-Thanh Sơn-Phú Thọ</span>
+        </div>
+    </div>
+
+    <div class="product-navs">
+        <ul>
+            <li>Trang Chủ</li>
+            <li>Sản Phẩm Khuyến Mãi</li>
+            <li>Iphone</li>
+            <li>SamSung</li>
+            <li>Báo Lỗi</li>
+            <li><input type="text" placeholder="Nhập vào đây..."> <button><i class='bx bx-search-alt-2' ></i></button></li>
+        </ul>
+    </div>
+
+    <div class="container">
+        <div class="slideshow">
+           <img src="./img/sl1.jpg" alt="">
+           <img src="./img/sl2.jpg" alt="">
+        </div>
+
+        <div class="banner-bottom">
+            <img src="./img/bnbt.jpg">
+        </div>
+
+        <div class="title">
+            <h2>Sản Phẩm Khuyến Mãi</h2>
+        </div>
+        <div class="list-container">
+            <ul>
+                <li>
+                    <img src="./img/p1.jpg">
+                    <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span>
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+            </ul>
+        </div>
+        <!--Iphone-->
+        <div class="title">
+            <h2>IPHONE</h2>
+        </div>
+        <div class="list-container">
+            <ul>
+                <li>
+                    <img src="./img/p1.jpg">
+                    <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span>
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+            </ul>
+        </div>
+        <!--Page2-->
+        <div class="list-container">
+            <ul>
+                <li>
+                    <img src="./img/p1.jpg">
+                    <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span>
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+            </ul>
+        </div>
+        <!--Sámung-->
+        <div class="title">
+            <h2>SAMSUNG</h2>
+        </div>
+        <div class="list-container">
+            <ul>
+                <li>
+                    <img src="./img/p1.jpg">
+                    <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span>
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+            </ul>
+        </div>
+        <!--Page2-->
+        <div class="list-container">
+            <ul>
+                <li>
+                    <img src="./img/p1.jpg">
+                    <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span>
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+            </ul>
+        </div>
+        <!--Máy tính bảng-->
+        <div class="title">
+            <h2>MÁY TÍNH BẢNG</h2>
+        </div>
+        <div class="list-container">
+            <ul>
+                <li>
+                    <img src="./img/p1.jpg">
+                    <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span>
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+            </ul>
+        </div>
+        <!--Phụ Kiện Chính Hãng-->
+        <div class="title">
+            <h2>PHỤ KIỆN CHÍNH HÃNG</h2>
+        </div>
+        <div class="list-container">
+            <ul>
+                <li>
+                    <img src="./img/p1.jpg">
+                    <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span>
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+            </ul>
+        </div>
+        <!--Page2-->
+        <div class="list-container">
+            <ul>
+                <li>
+                    <img src="./img/p1.jpg">
+                    <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span>
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+                <li>
+                <img src="./img/p1.jpg">
+                <span>IPHONE 15</span> 
+                    <span>24.000.000<sup>đ</sup></span>
+                </li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="footer">
+    <hr style="color: #2B3F9D;">
+        <ul>
+            <li>
+                <h3>KẾT NỐI VỚI CHÚNG TÔI</h3>
+                <hr style="margin-bottom: 10px;">
+                <i style="margin-top: 5px;" class='bx bxl-facebook-square'></i><span>FaceBook</span> <br>
+                <i style="margin-top: 5px;" class='bx bxl-youtube' ></i><span>YouTube</span>  <br>
+                <i style="margin-top: 5px;" class='bx bxl-instagram-alt' ></i><span>Instagram</span><br>
+                <img src="./img/dathongbaobct_150.png" style="margin-top: 10px;">
+            </li>
+            <li>
+                <h3>TRỢ GIÚP MUA HÀNG</h3>
+                <hr style="margin-bottom: 10px;">
+                <i style="color: #9999" class='bx bx-chevron-right' ></i><span>Chính sách thanh toán</span> <br>
+                <i style="color: #9999" class='bx bx-chevron-right' ></i><span>Chính sách khiếu nại</span> <br>
+                <i style="color: #9999" class='bx bx-chevron-right' ></i><span>Chính sách vận chuyển</span> <br>
+                <i style="color: #9999" class='bx bx-chevron-right' ></i><span>Chính sách đổi trả</span> <br>
+                <i style="color: #9999" class='bx bx-chevron-right' ></i><span>Chính sách bảo hành</span>
+
+            </li>
+            <li>
+                <h3>LIÊN KẾT VÀ HỖ TRỢ</h3>
+                <hr>
+                <h5 style="margin-left: 5px ; margin-top: 10px;">Hộ kinh doanh Xuân Hoài</h5>
+                <p>MST 0314217652 do UBND  H. Bình Chánh  cấp ngày 19/01/2017</p>
+            </li>
+            <li>
+                <h3>ĐĂNG KÝ NHẬN TIN</h3>
+                <hr>
+                <input style="margin-top: 20px;" type="text" placeholder="Nhập vào đây..." > <button>Gửi</button>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
